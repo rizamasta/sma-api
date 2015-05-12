6d4a120288bc7c4583b84cc4d0292e7b78ebb6af
leomastakusuma <leomastakusuma@gmail.com>
2015-05-12 19:43:28 +0700

    Update Function Login

--- Gateway/application/modules/account/controllers/LoginController.php
+++ Gateway/application/modules/account/controllers/LoginController.php
@@ -3,14 +3,13 @@
 class Account_LoginController extends Application_Model_DealerAbstract{
 
 
-    protected function getModelLogin(){
-        $model = new Account_Model_Account();
-        return $model;
+    public function init(){
+        $this->view->Login  = true;
+        $this->getDataUsers();
     }
 
     public function indexAction(){
-        $this->_validParams();
-        $this->getDataUser();
+
 
     }
 
@@ -18,36 +17,46 @@ class Account_LoginController extends Application_Model_DealerAbstract{
     protected function  _validParams(){
         $params = $this->_request->getParams();
         if(!empty($params['username']) && is_numeric($params['username'])){
-            $this->view->errorMsg = 'Username Can`t Be Numeric';
+            $this->view->errorMsgLogin = 'Username Can`t Be Numeric';
 
         }
 
     }
 
-    protected function getDataUser(){
+
+    protected function _getDataUser(){
         $params = $this->_request->getParams();
 
-        try{
-            $response= $this->getModelLogin()->getUser($params['username'],sha1($params['password']));
-            /* Start Update Data Login User */
-            if(!empty($response)){
-                try{
-                    $upadateUser = array(
-                        'last_log'=>new Zend_Db_Expr('NOW()')
-                    );
-                    $where = $this->getModelLogin()->getAdapter()->quoteInto('username = ?',$response['username']);
-                    $this->getModelLogin()->update($upadateUser,$where);
-                }catch (Exception $e){
-                    $this->view->errorMsg=$e->getMessage();
+            try{
+                if(!empty($params['username']) && $params['password'])
+                 $response= $this->getModelLogin()->getUser($params['username'],sha1($params['password']));
+
+                /* Start Update Data Login User*/
+                if(!empty($response)){
+                    try{
+                        $upadateUser = array(
+                            'last_log'=>new Zend_Db_Expr('NOW()')
+                        );
+
+                        if(!empty($response['username'])){
+                            $where = $this->getModelLogin()->getAdapter()->quoteInto('username = ?',$response['username']);
+                            $this->getModelLogin()->update($upadateUser,$where);
+                        }
+
+                    }catch (Exception $e){
+                        $this->view->errorMsg=$e->getMessage();
+                    }
+
                 }
+                /* End Update Data User*/
+                $this->view->ResponseData = $response;
+//                return $this->_getToken($response);
 
+            }catch (Exception $e){
+                $this->view->msg= json_encode($e->getMessage());
             }
-            /* End Update Data User */
-            echo json_encode($response);
 
-        }catch (Exception $e){
-            echo json_encode($e->getMessage());
-        }
+
 
     }
 }
